bc3960d4417e0bab50c77b28bfd3dec9b4b636ae
dharaneswarang <dharanesh86688@gmail.com>
2023-10-02 11:29:14 +0530

    Initial Commit

---
+++ FLAMES/FLAMESApp.swift
@@ -0,0 +1,17 @@
+//
+//  FLAMESApp.swift
+//  FLAMES
+//
+//  Created by Dharaneswaran G on 02/10/23.
+//
+
+import SwiftUI
+
+@main
+struct FLAMESApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
